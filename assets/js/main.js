5acf18364408eadc5f9c5213979d33eb7fffda39
mrshiam <mrshiam01@gmail.com>
2021-11-18 11:15:34 +0600

    fifth update

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,3 +129,23 @@
   }
     }
     document.addEventListener("scroll", handleScroll)
+
+    // TAB WIDGET
+    var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+
+//INDIVIDUAL
+var triggerEl = document.querySelector('#myTab a[href="#profile"]')
+bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
+
+var triggerFirstTabEl = document.querySelector('#myTab li:first-child a')
+bootstrap.Tab.getInstance(triggerFirstTabEl).show() // Select first tab
+
+//
